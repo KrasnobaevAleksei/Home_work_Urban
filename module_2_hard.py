c399075fd89ee27687e5805ebb8d0bb14f0e75ba
Krasnobaev_Aleksey <64255796+KrasnobaevAleksei@users.noreply.github.com>
2024-08-29 21:53:12 +0500

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,11 @@
+list_  = []
+n = int(input('Число из первой вставки : '))
+
+for i in range(1, n):
+    for j in range(i+1, n):
+        if n % (i+j) == 0:
+            list_.append(i)
+            list_.append(j)
+        else:
+            continue
+print(*list_)
