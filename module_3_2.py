5d31d4e928dcbea3fd7e85dc7a014984503b4b11
Aleksei <krasnobaev07@inbox.ru>
2024-09-04 22:03:44 +0500

    first commit

---
+++ module_3_2.py
@@ -0,0 +1,26 @@
+def send_email (message, recipient, sender = "university.help@gmail.com"):
+    if ("@" in recipient and (check_end( recipient, ".com") or
+                             check_end( recipient,  ".ru") or
+                             check_end( recipient, ".net")) and
+                             (check_end( sender, ".com") or
+                             check_end( sender, ".ru") or
+                             check_end( sender, ".net"))) :
+        if recipient == sender:
+            print_ = (f'Нельзя отправить письмо самому себе')
+        elif sender != "university.help@gmail.com":
+            print_ = (f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса '
+                       f'{sender} на адрес {recipient}.')
+        else:
+            print_ = (f'Письмо отправлено с адреса {sender} на адрес {recipient}')
+    else:
+        print_ = (f'Невозможно отправить c адреса {sender} на адрес {recipient}')
+    return print(print_)
+def check_end(check_mail,  str_):
+    res = False
+    if check_mail.endswith(str_):
+        res = True
+        return res
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
